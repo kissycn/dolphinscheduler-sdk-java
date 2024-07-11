8a7d73bc38420847fdf52d4922b32aa40dda3594
Jerry.Zhou <kissycn@gmail.com>
2024-07-11 17:24:19 +0800

    feat: add task instance log detail rest api and operator

---
+++ src/main/java/com/github/weaksloth/dolphins/instance/LogDetailResp.java
@@ -0,0 +1,10 @@
+package com.github.weaksloth.dolphins.instance;
+
+import lombok.Data;
+
+/** @create: 2024-07-11 17:14 */
+@Data
+public class LogDetailResp {
+  private int lineNum;
+  private String message;
+}
--- src/main/java/com/github/weaksloth/dolphins/instance/TaskInstanceOperator.java
+++ src/main/java/com/github/weaksloth/dolphins/instance/TaskInstanceOperator.java
@@ -10,47 +10,67 @@ import com.github.weaksloth.dolphins.remote.DolphinsRestTemplate;
 import com.github.weaksloth.dolphins.remote.HttpRestResult;
 import com.github.weaksloth.dolphins.remote.Query;
 import com.github.weaksloth.dolphins.util.JacksonUtils;
-
 import java.util.Optional;
+import lombok.extern.slf4j.Slf4j;
 
-/**
- * @create: 2024-07-11 11:43
- **/
+/** @create: 2024-07-11 11:43 */
+@Slf4j
 public class TaskInstanceOperator extends AbstractOperator {
-    public TaskInstanceOperator(String dolphinAddress, String token, DolphinsRestTemplate dolphinsRestTemplate) {
-        super(dolphinAddress, token, dolphinsRestTemplate);
+  public TaskInstanceOperator(
+      String dolphinAddress, String token, DolphinsRestTemplate dolphinsRestTemplate) {
+    super(dolphinAddress, token, dolphinsRestTemplate);
+  }
+
+  /**
+   * page query task's instance list
+   *
+   * @param page page,default 1 while is null
+   * @param size size,default 10 while is null
+   * @param projectCode project code
+   * @param processInstanceName process instance name
+   * @return PageInfo<TaskInstanceQueryResp>
+   */
+  public PageInfo<TaskInstanceQueryResp> page(
+      Integer page, Integer size, Long projectCode, String processInstanceName) {
+    page = Optional.ofNullable(page).orElse(DolphinClientConstant.Page.DEFAULT_PAGE);
+    size = Optional.ofNullable(size).orElse(DolphinClientConstant.Page.DEFAULT_SIZE);
+
+    String url = dolphinAddress + "/projects/" + projectCode + "/task-instances";
+
+    Query query = new Query();
+    query
+        .addParam("pageNo", String.valueOf(page))
+        .addParam("pageSize", String.valueOf(size))
+        .addParam("processInstanceName", processInstanceName);
+
+    try {
+      HttpRestResult<JsonNode> restResult =
+          dolphinsRestTemplate.get(url, getHeader(), query, JsonNode.class);
+      return JacksonUtils.parseObject(
+          restResult.getData().toString(), new TypeReference<PageInfo<TaskInstanceQueryResp>>() {});
+    } catch (Exception e) {
+      throw new DolphinException("page dolphin scheduler process instance list fail", e);
+    }
+  }
+
+  public LogDetailResp logDetail(Long taskInstanceId, Integer limit, Integer skipLineNum) {
+    String url = dolphinAddress + "/log/detail";
+    log.info("get process task definition detail, url:{}", url);
+
+    Query query =
+        new Query()
+            .addParam("taskInstanceId", String.valueOf(taskInstanceId))
+            .addParam("limit", String.valueOf(limit))
+            .addParam("skipLineNum", String.valueOf(skipLineNum));
+
+    HttpRestResult<LogDetailResp> restResult =
+        dolphinsRestTemplate.get(url, getHeader(), query, LogDetailResp.class);
+
+    if (restResult.getFailed()) {
+      log.error("dolphin scheduler response:{}", restResult);
+      throw new DolphinException(restResult.getCode(), restResult.getMsg());
     }
 
-    /**
-     * page query task's instance list
-     *
-     * @param page         page,default 1 while is null
-     * @param size         size,default 10 while is null
-     * @param projectCode  project code
-     * @param processInstanceName process instance name
-     * @return PageInfo<TaskInstanceQueryResp>
-     */
-    public PageInfo<TaskInstanceQueryResp> page(
-            Integer page, Integer size, Long projectCode, String processInstanceName) {
-        page = Optional.ofNullable(page).orElse(DolphinClientConstant.Page.DEFAULT_PAGE);
-        size = Optional.ofNullable(size).orElse(DolphinClientConstant.Page.DEFAULT_SIZE);
-
-        String url = dolphinAddress + "/projects/" + projectCode + "/task-instances";
-
-        Query query = new Query();
-        query
-                .addParam("pageNo", String.valueOf(page))
-                .addParam("pageSize", String.valueOf(size))
-                .addParam("processInstanceName", processInstanceName);
-
-        try {
-            HttpRestResult<JsonNode> restResult =
-                    dolphinsRestTemplate.get(url, getHeader(), query, JsonNode.class);
-            return JacksonUtils.parseObject(
-                    restResult.getData().toString(),
-                    new TypeReference<PageInfo<TaskInstanceQueryResp>>() {});
-        } catch (Exception e) {
-            throw new DolphinException("page dolphin scheduler process instance list fail", e);
-        }
-    }
+    return restResult.getData();
+  }
 }
